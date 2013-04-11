5c1790ef03cf4c48744e29c640ae0708df5210a3
Scott Dickey <scott@grokh.com>
2013-04-11 21:49:59 +0000

    Updated makehome to do interactive copy of all dotfiles

--- makehome.sh
+++ makehome.sh
@@ -1,2 +1,9 @@
+cp -i .bashrc ~/
+cp -i .bash_profile ~/
+cp -i .vimrc ~/
+cp -i .gitconfig ~/
+cp -i .gitignore_global ~/
+cp -i .tmux.conf ~/
+cp -ir .vim ~/
 mkdir -p ~/.vim/backup
-mkdir ~/.vim/tmp
+mkdir -p ~/.vim/tmp
