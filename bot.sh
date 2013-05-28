2e1adf1e3f5504f3a3af859d2e0942c171e0f447
Scott Dickey <scott@grokh.com>
2013-05-28 13:21:44 -0400

    Fix directory issue

--- bot.sh
+++ bot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dir=/srv/bot
 tmux new-session -d -s kat "cd $dir;tt++ katumi.tin"
-tmux split-window -h -p 40 -t kat "tail -f logs/tells.log"
-tmux split-window -v -p 50 -t kat "tail -f logs/bot.log"
+tmux split-window -h -p 40 -t kat "cd $dir;tail -f logs/tells.log"
+tmux split-window -v -p 50 -t kat "cd $dir;tail -f logs/bot.log"
 
 tmux select-pane -t 0
 tmux -2 attach-session -t kat
--- tm.sh
+++ tm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 dir=$HOME/mud/torilmud
 tmux new-session -d -s toril "cd $dir;tt++ basic.tin"
-tmux split-window -h -p 40 -t toril "tail -f chat.log"
-tmux split-window -v -p 50 -t toril "tail -f groupinfo.log"
+tmux split-window -h -p 40 -t toril "cd $dir;tail -f chat.log"
+tmux split-window -v -p 50 -t toril "cd $dir;tail -f groupinfo.log"
 
 tmux select-pane -t 0
 tmux -2 attach-session -t toril
