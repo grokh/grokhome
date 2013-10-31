5817aea87e659d856c5013f020c103e9af5e3997
Scott Dickey <scott@grokh.com>
2013-10-31 13:30:39 +0000

    Comment out tm.sh and gobot links

--- makehome.sh
+++ makehome.sh
@@ -8,8 +8,8 @@ cp -i .tmux.conf ~/
 cp -ir .vim ~/
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/tmp
-sudo ln -s $HOME/files/grokhome/tm.sh /usr/local/bin/tm
+#sudo ln -s $HOME/files/grokhome/tm.sh /usr/local/bin/tm
 sudo ln -s $HOME/files/grokhome/bot.sh /usr/local/bin/bot
-sudo ln -s $HOME/go/bin/gobot /usr/local/bin/gobot
+#sudo ln -s $HOME/go/bin/gobot /usr/local/bin/gobot
 sudo iptables-restore < iptables
 sudo /sbin/service iptables save
